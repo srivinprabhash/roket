dcb814ca08a3472dfac58d89628debd50a902ab6
Srivin Prabhash <me@srivin.me>
2022-08-16 14:04:27 +0530

    better docs and cleanup

--- cmd/new.go
+++ cmd/new.go
@@ -26,11 +26,13 @@ var newCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 
+		// Concatname migration dir name
 		path := viper.GetViper().GetString("migrations")
 		unixtime := strconv.FormatInt(time.Now().Unix(), 10)
 		migrationName := unixtime + "_" + args[0]
 		path = path + "/" + migrationName
 
+		// Create migration dir
 		if err := os.Mkdir(path, 0744); err != nil {
 			fmt.Println(err)
 			return
@@ -52,14 +54,4 @@ var newCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(newCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// newCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// newCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
